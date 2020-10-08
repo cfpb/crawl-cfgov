e5e4674086811180f7e0362ecc94d6caac1c7f31
Betsy Lorton <elizabeth.lorton@cfpb.gov>
2020-10-08 18:39:04 -0400

    Temporarily reduce crawl depth to test whether the crawl finishes

--- crawl.sh
+++ crawl.sh
@@ -8,7 +8,7 @@ time wget \
     --reject '*.css,*.doc,*.docx,*.gif,*.ico,*.jpg,*.js,*.mp3,*.pdf,*.png,*.txt,*.wav,*.woff,*.woff2,*.xls,*xlsx,*.zip' \
     --reject-regex "topics=|authors=|categories=|filter_blog_category=|ext_url=|search_field=|issuer_name=" \
     --recursive \
-    --level=inf \
+    --level=140 \
     --trust-server-names \
     --no-verbose \
     --no-clobber \
