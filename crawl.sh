c7b175fd1a95093abb09bb74a55b74a73c4146a9
Andy Chosak <andy.chosak@cfpb.gov>
2020-10-09 12:00:37 -0400

    Ignore .epubs and .pptxs

--- crawl.sh
+++ crawl.sh
@@ -5,7 +5,7 @@ time wget \
     --follow-tags=a \
     --limit-rate=200k \
     --random-wait \
-    --reject '*.css,*.doc,*.docx,*.gif,*.ico,*.jpg,*.js,*.mp3,*.pdf,*.png,*.txt,*.wav,*.woff,*.woff2,*.xls,*xlsx,*.zip' \
+    --reject '*.css,*.doc,*.docx,*.epub,*.gif,*.ico,*.jpg,*.js,*.mp3,*.pdf,*.png,*.pptx,*.txt,*.wav,*.woff,*.woff2,*.xls,*xlsx,*.zip' \
     --reject-regex "topics=|authors=|categories=|filter_blog_category=|ext_url=|search_field=|issuer_name=" \
     --recursive \
     --level=3 \
