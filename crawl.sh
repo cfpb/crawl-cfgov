ab07cdf80b13db098f3f04fbac68b490b6628d15
Andy Chosak <andy.chosak@cfpb.gov>
2020-10-29 16:48:00 -0400

    Shell usability improvements, plus logging
    This commit tries to improve the usability of the shell script so that
    it can be iteratively invoked from the command line without requiring
    manual edits.
    
    It can now be invoked like:
    
    ./crawl.sh https://www.consumerfinance.gov
    ./crawl.sh https://www.consumerfinance.gov/start/at/specific/page/
    
    or, to limit its depth to an arbitrary level:
    
    ./crawl.sh -d 4 https://www.consumerfinance.gov
    
    This should be helpful for testing and development, for example when
    running against localhost, starting at a different URL, or using
    different depths.
    
    In addition, the shell now logs wget output to a wget.log logfile, while
    still outputting to console. It also logs rejected URLs to rejected.log
    using wget's --rejected.log option. This is helpful to debug why certain
    URLs were skipped (unfortunately, this file also logs the 2nd, 3rd, nth
    time a URL is visited as a "BLACKLIST" rejection even if it was already
    crawled).
    
    I've also increased --limit-rate to 1 MB from 200 KB/sec, and removed
    --random-wait, which wasn't doing anything since we aren't specifying
    --wait.

--- crawl.sh
+++ crawl.sh
@@ -1,16 +1,73 @@
 #!/usr/bin/env bash
+
+# Recursively crawl a website and save its HTML locally.
+#
+# Example usage:
+#
+# ./crawl.sh [-d depth] https://www.consumerfinance.gov
+#
+# Optionally specify -d depth to limit the crawl depth.
+
+# Fail if anything fails.
+set -e
+
+depth=0
+
+while getopts ":d:" opt; do
+    case $opt in
+      d )
+        depth="$OPTARG";
+        number_regex='^[0-9]+$'
+        if ! [[ $depth =~ $number_regex ]] ; then
+            echo "Crawl depth must be a number." 1>&2
+            exit 1
+        fi
+        ;;
+    \? )
+        echo "Invalid option: -$OPTARG." 1>&2
+        exit 1
+        ;;
+    : )
+        echo "Invalid option: -$OPTARG requires an argument." 1>&2
+        exit 1
+        ;;
+    esac
+done
+
+shift $((OPTIND -1))
+
+url=$1
+
+if [ -z "$url" ]; then
+  echo "Must specify URL to crawl."
+  exit 1
+fi
+
+echo "Starting crawl at $url."
+
+if [ $depth -ne 0 ]; then
+    echo "Limiting crawl to depth $depth."
+fi
+
+domain=$url
+domain="${domain#http://}"
+domain="${domain#https://}"
+domain="${domain%%:*}"
+domain="${domain%%\?*}"
+domain="${domain%%/*}"
+echo "Limiting crawl to domain $domain."
+
 time wget \
-    --domains=www.consumerfinance.gov \
-    --exclude-domains=files.consumerfinance.gov \
+    --domains="$domain" \
     --execute robots=off \
     --follow-tags=a \
-    --limit-rate=200k \
-    --random-wait \
+    --limit-rate=1m \
     --accept html \
     --reject-regex "topics=|authors=|categories=|filter_blog_category=|ext_url=|search_field=|issuer_name=" \
     --recursive \
-    --level=4 \
+    --level="$depth" \
     --trust-server-names \
-    --no-verbose \
+    --verbose \
     --no-clobber \
-    https://www.consumerfinance.gov/
+    --rejected-log=rejected.log \
+    "$url" 2>&1 | tee wget.log
