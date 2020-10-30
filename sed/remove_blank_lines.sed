05577279c618a04105f4ca0e70cebdee197f22c6
Betsy Lorton <elizabeth.lorton@cfpb.gov>
2020-10-30 14:28:42 -0400

    More transparent sed command

---
+++ sed/remove_blank_lines.sed
@@ -0,0 +1,6 @@
+#########
+# This sed command will remove all lines that are empty or only contain whitespace
+#########
+
+#  delete blank lines
+/^[[:space:]]*$/d
---
+++ transform_results.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# for each file in the www.consumerfinance.gov directory, run the given sed commands on it
+find www.consumerfinance.gov -type f \
+    -exec sed -f 'sed/remove_scripts.sed' -f 'sed/remove_blank_lines.sed' -i '' {} \;
