0c18cf7db7a5345d2a314f53f15a590e7dc9e4f1
Andy Chosak <andy.chosak@cfpb.gov>
2020-10-30 10:54:33 -0400

    Try simpler method of commiting back
    Using an environment variable with EndBug/add-and-commit seems to have
    trouble with excessively long commit messages. Try using a file instead,
    and just pushing back directly.
    
    Also makes a small change to the commit message to include the date and
    ensure we get the full filenames of changed files.

--- generate_summary.sh
+++ generate_summary.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 
-# Generate a summary of the crawl results into message.txt
-git add -A www.consumerfinance.gov
-git diff --staged --compact-summary --no-color > message.txt
-git reset .
+# Fail if anything fails.
+set -e
 
-# Follow these instructions to set the value of the COMMIT_MESSAGE
-# environment variable and save it to the GitHub Actions environment:
-# https://docs.github.com/en/free-pro-team@latest/actions/reference/workflow-commands-for-github-actions#setting-an-environment-variable
-echo 'COMMIT_MESSAGE<<EOF' >> $GITHUB_ENV
-echo $(cat message.txt) >> $GITHUB_ENV
-echo 'EOF' >> $GITHUB_ENV
+# Generate a summary of the crawl results into commit.txt
+date > commit.txt
+git diff --numstat --no-color -- '*.html' >> commit.txt
