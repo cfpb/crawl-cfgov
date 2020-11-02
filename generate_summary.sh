d6e397d7ab726140ccaed0598b4a7c38bcca58ae
Andy Chosak <andy.chosak@cfpb.gov>
2020-11-02 10:35:26 -0500

    Add header to commit.txt

--- generate_summary.sh
+++ generate_summary.sh
@@ -5,4 +5,9 @@ set -e
 
 # Generate a summary of the crawl results into commit.txt
 date > commit.txt
+cat >> commit.txt <<EOL
+lines  |lines  |
+added  |deleted|filename
+-------|-------|--------
+EOL
 git diff --numstat --no-color -- '*.html' >> commit.txt
