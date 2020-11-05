44eb481305de818817290e728cc3220b7105089e
Betsy Lorton <elizabeth.lorton@cfpb.gov>
2020-11-05 15:48:05 -0500

    Prevent sed error in logs
    GH Actions uses GNU sed, which has slightly different syntax from MacOS
    sed. For GNU sed, the -i flag does not take an argument.

--- transform_results.sh
+++ transform_results.sh
@@ -2,4 +2,4 @@
 
 # for each file in the www.consumerfinance.gov directory, run the given sed commands on it
 find www.consumerfinance.gov -type f \
-    -exec sed -f 'sed/remove_scripts.sed' -f 'sed/remove_blank_lines.sed' -i '' {} \;
+    -exec sed -f 'sed/remove_scripts.sed' -f 'sed/remove_blank_lines.sed' -i {} \;
