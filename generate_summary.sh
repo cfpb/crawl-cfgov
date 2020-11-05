6e8a0d80a9d37e5ef602e082983b0b5d55e9bdbf
Andy Chosak <andy.chosak@cfpb.gov>
2020-11-05 15:05:57 -0500

    Improve summary by diffing all intended files
    This change modifies the generate_summary.sh script so that it takes an
    argument that can be used to indicate which files to diff. Previously,
    only HTML files were being diffed, but due to long URLs, wget may
    download files that end in other extensions.
    
    Additionally, this changes the workflow file so that we only define the
    domain to crawl in a single place.

--- generate_summary.sh
+++ generate_summary.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
 
+# Generate a friendly diff message for a set of HTML files.
+#
+# Exampleusage:
+#
+# ./generate_summary.sh path_to_html
+
 # If a command fails, stop executing this script and return its error code.
 set -e
+
+crawl_root=$1
+
+if [ -z "$crawl_root" ]; then
+    echo "Must specify path to HTML."
+    exit 1
+fi
 
 # Generate a summary of the crawl results into commit.txt
 date > commit.txt
@@ -10,4 +23,4 @@ lines  |lines  |
 added  |deleted|filename
 -------|-------|--------
 EOL
-git diff --numstat --no-color -- '*.html' >> commit.txt
+git diff --cached --numstat --no-color "$crawl_root" >> commit.txt
